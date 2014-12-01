55dd588b28ce61aa8b58f70b545f9c1bfefd42f2
Srivathsan Murali <srivathsan92@gmail.com>
2014-12-01 10:37:12 +0100

    got csv reading working

---
+++ csvfuncs.py
@@ -0,0 +1,23 @@
+import csv
+import collections
+
+
+def readTrainingData():
+	trainingData = collections.namedtuple('trainingData',['cities','cityCodes','countryCodes']);
+	with open('training.csv','rb') as csvfile:
+		trainingreader = csv.reader(csvfile,delimiter=',');
+		trainingData.cities = list();
+		trainingData.cityCodes = list();
+		trainingData.countryCodes = list();
+		for row in trainingreader:
+			trainingData.cities.append(row.pop(0))
+			trainingData.cityCodes.append(row.pop(0))
+			trainingData.countryCodes.append(row.pop(0))
+		
+	return trainingData;
+
+td = readTrainingData();
+print len(td.cities)
+print td.cities[0]
+print td.cityCodes[0]
+print td.countryCodes[0]
