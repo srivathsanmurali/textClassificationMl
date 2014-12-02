1d94c70a8773a99b5b779b89e73d99e857e9dbf9
Srivathsan Murali <srivathsan92@gmail.com>
2014-12-02 14:43:29 +0100

    Adaboost gave horrible results

--- main_adaboost.py
+++ main_adaboost.py
@@ -1,6 +1,6 @@
 import csvfuncs as cf
 import bagOfWords as bow
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier
 
 td = cf.readTrainingData();
 #trainXRaw = td.cities;
@@ -13,21 +13,21 @@ trainY2   = td.countryCodes
 #validXBow = bow.getBOW(validXRaw,wordList,'validation')
 validXBow = bow.getBOWFromFile('validation_BOW.csv')
 
-print "training a KNN classifier with the training data for Y1"
-clfY1 = KNeighborsClassifier(n_neighbors =3)
-clfY1.fit(trainXBow,Y1)
-print "predicting Y1 using the KNN classifier"
+print "training a AdaBoost classifier with the training data for Y1"
+clfY1 = AdaBoostClassifier(n_estimators=100)
+clfY1.fit(trainXBow,trainY1)
+print "predicting Y1 using the AdaBoost classifier"
 Y1_hat = clfY1.predict(validXBow)
 
-print "training a KNN classifier with the training data for Y2"
-clfY2 = KNeighborsClassifier(n_neighbors =3)
-clfY2.fit(trainXBow,Y2)
-print "predicting Y2 using the KNN classifier"
+print "training a AdaBoost classifier with the training data for Y2"
+clfY2 = AdaBoostClassifier(n_estimators=100)
+clfY2.fit(trainXBow,trainY2)
+print "predicting Y2 using the AdaBoost classifier"
 Y2_hat = clfY2.predict(validXBow)
 
 print "prediction complete"
 
-f = open('validation_result_KNN.csv','w')
+f = open('validation_result_AdaBoost.csv','w')
 for i in range(len(Y1_hat)):
 	str1 = str(int(Y1_hat[i])) + "," + str(int(Y2_hat[i])) + "\n"
 	f.write(str1)
