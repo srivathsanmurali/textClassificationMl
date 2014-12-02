5d1f97b203810ecd393e65e22924d8afc2289083
Srivathsan Murali <srivathsan92@gmail.com>
2014-12-02 14:56:44 +0100

    random forest classification, this has the best results so far

---
+++ main_random_forrest.py
@@ -0,0 +1,36 @@
+import csvfuncs as cf
+import bagOfWords as bow
+from sklearn.ensemble import RandomForestClassifier
+
+td = cf.readTrainingData();
+#trainXRaw = td.cities;
+trainXBow = bow.getBOWFromFile('training_BOW.csv')
+trainY1   = td.cityCodes
+trainY2   = td.countryCodes
+#wordList = bow.getWordListFromCSV('training_wordList.csv')
+
+#validXRaw = cf.readXData('validation.csv')
+#validXBow = bow.getBOW(validXRaw,wordList,'validation')
+validXBow = bow.getBOWFromFile('validation_BOW.csv')
+
+print "training a RandomForestClassifier with the training data for Y1"
+clfY1 = RandomForestClassifier(n_estimators=10)
+clfY1.fit(trainXBow,trainY1)
+print "predicting Y1 using the RandomForestClassifier"
+Y1_hat = clfY1.predict(validXBow)
+
+print "training a RandomForestClassifier with the training data for Y2"
+clfY2 = RandomForestClassifier(n_estimators=10)
+clfY2.fit(trainXBow,trainY2)
+print "predicting Y2 using the RandomForestClassifier"
+Y2_hat = clfY2.predict(validXBow)
+
+print "prediction complete"
+
+f = open('validation_result_RandomForest.csv','w')
+for i in range(len(Y1_hat)):
+	str1 = str(int(Y1_hat[i])) + "," + str(int(Y2_hat[i])) + "\n"
+	f.write(str1)
+
+f.close()
+
